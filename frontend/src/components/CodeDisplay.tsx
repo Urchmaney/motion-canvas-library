dce54b1d983b29e9f27629f3f9e452fd78b2c8fa
Unegbu Kingsley <kingsobino@gmail.com>
2024-10-29 12:22:50 +0100

    add library code

---
+++ frontend/src/components/CodeDisplay.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react"
+import { CopyIcon } from "./icons"
+
+export const CodeDisplay = ({ code }: { code: string }) => {
+  const [copied, setCopied] = useState(false);
+  const copyTopClipboard = () => {
+    navigator.clipboard.writeText(code);
+    setCopied(true);
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCopied(false)
+    }, 1000)
+  }, [copied])
+
+
+  return (
+    <div className="">
+      <div className="flex justify-end p-2 py-4">
+        <span className="cursor-pointer" onClick={copyTopClipboard}><CopyIcon size={24} fill={copied ? "#000": ""} /></span>
+      </div>
+      <div className="w-full overflow-x-scroll overflow-y-scroll relative py-3 px-5 pt-2 bg-no-repeat  bg-cover h-[1000px] max-h-[700px]">
+        <pre className="w-[600px]">
+          {code}
+        </pre>
+      </div>
+    </div>
+  )
+}
