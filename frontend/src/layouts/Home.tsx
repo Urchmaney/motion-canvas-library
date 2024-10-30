846647dc908c7b96a0e8ead008d06b3d3a8c766e
Unegbu Kingsley <kingsobino@gmail.com>
2024-10-30 16:03:57 +0100

    change curtain position to layout

--- frontend/src/layouts/Home.tsx
+++ frontend/src/layouts/Home.tsx
@@ -5,6 +5,13 @@ import { MotionCanvasLibraryIcon } from "../components/icons";
 export default function HomeLayout() {
   return (
     <div>
+      <div className="absolute h-screen w-32 bg-right-curtain bg-contain bg-no-repeat left-0 animate-upward top-0 -z-10">
+
+      </div>
+
+      <div className="absolute h-screen w-32 bg-left-curtain bg-contain bg-no-repeat right-0 animate-upward top-0 -z-10">
+
+      </div>
       <div className="container mx-auto px-2">
         <div className="py-3 flex justify-between sticky top-0 z-10 bg-primary border-b border-[#E2E9ED]">
           <div className="flex items-center gap-8">
@@ -27,6 +34,7 @@ export default function HomeLayout() {
             <Link to={`https://github.com/Urchmaney/motion-canvas-library`} target="_blank"><GithubIcon size={30} /></Link>
           </div>
         </div>
+
         <div className="pt-5">
           <Outlet />
         </div>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,7 @@
 export default function Home () {
   return (
     <div className="h-[calc(100vh-100px)] -top-4">
-      <div className="absolute h-screen w-32 bg-right-curtain bg-contain bg-no-repeat left-0 animate-upward top-0">
-
-      </div>
-
-      <div className="absolute h-screen w-32 bg-left-curtain bg-contain bg-no-repeat right-0 animate-upward top-0">
-
-      </div>
+      
       
     </div>
   )
