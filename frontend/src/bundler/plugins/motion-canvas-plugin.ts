bb943a9761fb2fbfc66b538d1921c24873baf4eb
Kingsley uche <kingsobino@gmail.com>
2024-11-10 16:40:34 +0100

    split bundle between motion canvas 2d and core

--- frontend/src/bundler/plugins/motion-canvas-plugin.ts
+++ frontend/src/bundler/plugins/motion-canvas-plugin.ts
@@ -1,20 +1,22 @@
 import * as esbuild from 'esbuild-wasm';
 
+
 export default function motionCanvasPlugin() {
   return ({
     name: 'motion-canvas',
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /@motion-canvas\/2d$/ }, (_) => {
-        const url = new URL("../motion-canvas-2d.js", window.location.href);
-        return { path: url.href, namespace: 'a-2d' };
+      build.onResolve({ filter: /^(@motion-canvas\/2d|\.\/chunks\/|@motion-canvas\/core)/ }, (args: any) => {
+        let pathUrl = args.path;
+        if (pathUrl.match(/^\.\/chunks\//)) {
+          pathUrl = `@motion-canvas/${pathUrl.substring(1)}`
+        } else {
+          pathUrl = `${pathUrl}.js`
+        }
+        const url = new URL(`../${pathUrl}`, window.location.href);
+        return { path: url.href, namespace: 'motion-canvas' };
       });
 
-      build.onResolve({ filter: /@motion-canvas\/core$/ }, (_) => {
-        const url = new URL("../motion-canvas-core.js", window.location.href);
-        return { path: url.href, namespace: 'a-core' };
-      });
-
-      build.onLoad({ filter: /.*/, namespace: 'a-2d' }, async (args: any) => {
+      build.onLoad({ filter: /.*/, namespace: 'motion-canvas' }, async (args: any) => {
         try {
           const response = await fetch(args.path);
           const data = await response.text();
@@ -25,20 +27,6 @@ export default function motionCanvasPlugin() {
         } catch(e) {
           throw e
         }        
-      });
-
-      build.onLoad({ filter: /.*/, namespace: 'a-core' }, async (args: any) => {
-        try {
-          const response = await fetch(args.path);
-          const data = await response.text();
-          return {
-            contents: data,
-            loader: 'js',
-          };
-        }catch(e) {
-          throw e
-        }
-        
       });
     }
   })
