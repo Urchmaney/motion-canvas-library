73511bad5576371012196c7979e5370e9cf48923
Unegbu Kingsley <kingsobino@gmail.com>
2024-11-10 16:39:08 +0100

    split between motion canvas 2d and core

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,48 +7,34 @@ import esbuild from 'esbuild';
 export default defineConfig(
   {
     plugins: [react(),
-      {
-        name: 'bundle-and-copy-motion-canvas-core',
-        async buildStart() {
-          const entryFile = resolve(__dirname, '../node_modules/@motion-canvas/core/lib/index.js');
-          const outputFile = resolve('public/motion-canvas-core.js');
-  
-          try {
-            // Bundle the file with esbuild
-            await esbuild.build({
-              entryPoints: [entryFile],
-              outfile: outputFile,
-              bundle: true,
-              minify: true,
-              format: 'esm',
-            });
-            console.log(`Motion Canvas Core bundled into: ${outputFile}`);
-          } catch (error) {
-            console.error("Failed to bundle Motion Canvas Core :", error);
+    {
+      name: 'motion-canvas',
+      async buildStart() {
+        const entryFiles = [
+          {
+            in: resolve(__dirname, '../node_modules/@motion-canvas/2d/lib/index.js'), out: "2d",
+          },
+          {
+            in: resolve(__dirname, '../node_modules/@motion-canvas/core/lib/index.js'), out: "core"
           }
-        },
+        ]
+        const outputDir = resolve("public/@motion-canvas")
+        try {
+          await esbuild.build({
+            entryPoints: entryFiles,
+            outdir: outputDir,
+            bundle: true,
+            chunkNames: 'chunks/[name]-[hash]',
+            minify: true,
+            splitting: true,
+            format: 'esm',
+          });
+          console.log(`Motion Canvas bundled into: ${outputDir}`);
+        } catch (error) {
+          console.error("Failed to bundle the Motion Canvas:", error);
+        }
       },
-
-      {
-        name: 'bundle-and-copy-motion-canvas-2d',
-        async buildStart() {
-          const entryFile = resolve(__dirname, '../node_modules/@motion-canvas/2d/lib/index.js');
-          const outputFile = resolve('public/motion-canvas-2d.js');
-  
-          try {
-            await esbuild.build({
-              entryPoints: [entryFile],
-              outfile: outputFile,
-              bundle: true,
-              minify: true,
-              format: 'esm',
-            });
-            console.log(`Motion Canvas 2d bundled into: ${outputFile}`);
-          } catch (error) {
-            console.error("Failed to bundle the Motion Canvas 2d:", error);
-          }
-        },
-      },
+    },
     ]
   }
 )
