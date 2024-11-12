6051910e0f3c5dcb82cbaad8ae4ee212641b614d
Kingsley uche <kingsobino@gmail.com>
2024-11-12 09:06:50 +0100

    add firebase for getting nodes

---
+++ frontend/src/interfaces.ts
@@ -0,0 +1,13 @@
+export type CustomNode = {
+  id: string
+  name: string
+  description: string
+  code: string
+  numberOfCopies: number
+}
+
+export type CustomNodeCode = {
+  code: string
+  usage: string
+  node_id: string
+}
