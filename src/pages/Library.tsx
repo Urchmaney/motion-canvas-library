98aed3c4751dd5d846d67fa8432318d2af7b0340
Kingsley uche <kingsobino@gmail.com>
2024-11-28 12:03:40 +0100

    fix active viewing node

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -10,14 +10,22 @@ import { usePlayersContext } from "../contexts";
 
 
 
-function ComponentsListSidebar({ setSelectedNode }: { setSelectedNode: (id: string) => void }) {
+function ComponentsListSidebar({ setSelectedNode }: { setSelectedNode: (node: CustomNode) => void }) {
   const [customeNodes, setCustomNodes] = useState<CustomNode[]>([]);
+  const [activeId, setActiveId] = useState<string | undefined>();
+
   useEffect(() => {
     getCustomNodes().then(nodes => {
       setCustomNodes(nodes);
-      setSelectedNode(nodes[0]?.id)
+      setSelectedNode(nodes[0]);
+      setActiveId(nodes[0]?.id);
     })
-  }, [])
+  }, []);
+
+  const selectNode = (node: CustomNode) => {
+    setSelectedNode(node);
+    setActiveId(node.id);
+  }
   return (
     <div className="pe-3">
       <div>
@@ -38,9 +46,12 @@ function ComponentsListSidebar({ setSelectedNode }: { setSelectedNode: (id: stri
         <div className="text-gray-500 flex flex-col gap-3">
           {
             customeNodes.map((node, id) => (
-              <div className="flex justify-between cursor-pointer" onClick={() => setSelectedNode(node.id)} key={`custome_node_${id}`}>
-                {node.name}
-                <span className="text-xs rounded-md text-black bg-gray-300 flex items-center px-2">{node.numberOfCopies}</span>
+              <div className={`flex justify-between cursor-pointer`} onClick={() => selectNode(node)} key={`custome_node_${id}`}>
+                <div className="flex justify-start">
+                  <p className={`p-1 min-w-40  ${activeId === node.id ? "bg-[#D1D5DB] pe-6" : ""}`}>{node.name}</p>
+                  { activeId === node.id && <p className="h-full border-l-[50px] border-t-[33.5px] border-t-transparent border-l-secondary"></p> }
+                </div>
+                <span className="text-xs rounded-md text-black bg-secondary flex items-center px-2">{node.numberOfCopies}</span>
               </div>
             ))
           }
@@ -53,44 +64,44 @@ function ComponentsListSidebar({ setSelectedNode }: { setSelectedNode: (id: stri
 
 export default function Library() {
   const [section, setSection] = useState<"preview" | "code" | "usage">("preview")
-  const [customNodeId, setCustomNodeId] = useState<string | null>(null);
+  const [customNode, setCustomNode] = useState<CustomNode | null>(null);
   const [nodeCode, setNodeCode] = useState<CustomNodeCode | null>(null);
   const { players, addComponentPlayer } = usePlayersContext();
 
   const [nodePlayer, setNodePlayer] = useState<Player | null>(null);
 
   useEffect(() => {
-    if (customNodeId) {
+    if (customNode) {
       const setupNodePlayer = async (nodeCode: CustomNodeCode) => {
         const fullCode = combineCodes([nodeCode?.code || "", nodeCode?.usage || ""]);
         const scene = await createSceneFromCode(fullCode);
         const player = createPlayer(scene);
-        addComponentPlayer(customNodeId, player);
+        addComponentPlayer(customNode.id, player);
         setNodePlayer(player);
       }
 
-      const player = players[customNodeId];
+      const player = players[customNode.id];
       if (player) {
         setNodePlayer(player);
         return;
       }
-      getCustomNodeCode(customNodeId).then(code => {
+      getCustomNodeCode(customNode.id).then(code => {
         setNodeCode(code);
         return setupNodePlayer(code);
       }).then(
         _ => console.log("setup complete")).catch(_ => console.log("error setting up"));
     }
-  }, [customNodeId]);
+  }, [customNode?.id]);
 
   return (
     <div className="md:flex gap-14 md:pt-8">
       <div className="basis-1/4">
-        <ComponentsListSidebar setSelectedNode={(id: string) => setCustomNodeId(id)} />
+        <ComponentsListSidebar setSelectedNode={(node: CustomNode) => setCustomNode(node)} />
       </div>
       <div className="grow min-w-0 pt-10 md:pt-0">
-        <h2 className="font-bold text-2xl"> VSCode</h2>
+        <h2 className="font-bold text-2xl">{customNode?.name}</h2>
         <p className="text-gray-500 py-2 text-lg">
-          A visual studio code animation and abilities
+          {customNode?.description}
         </p>
 
         <div className="py-5">
